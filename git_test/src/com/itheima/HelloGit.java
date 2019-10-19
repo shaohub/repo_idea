372a41f316459b85c8676661b54561e8b05ebf9f
shao <dasanhou@163.com>
2019-10-19 11:13:16 +0800

    我的修改

--- git_test/src/com/itheima/HelloGit.java
+++ git_test/src/com/itheima/HelloGit.java
@@ -2,9 +2,8 @@ package com.itheima;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
+       int i = 1;
+        System.out.println(i);
     }
     public void test(){
         System.out.println(112);
